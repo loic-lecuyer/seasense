1dd9be0782196b1f7518396c8fd813634e987248
loic-lecuyer <lecuyer.location@gmail.com>
2022-03-14 19:25:46 +0100

    Crrection focus

--- Exavision.Seasense.Protocols.Seamos/Commands/Camera/ICameraFocusMinusRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Exavision.Seasense.Protocols.Seamos.Commands.Camera {
-    /// <summary>
-    /// Defines the <see cref="ICameraFocusMinusRequest" />.
-    /// </summary>
-    public interface ICameraFocusMinusRequest : ISeamosCommand
-    {
-        byte Speed { get; set; }
-    }
-}
--- Exavision.Seasense.Protocols.Seamos/Commands/Camera/ICameraFocusPlusRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Exavision.Seasense.Protocols.Seamos.Commands.Camera {
-    /// <summary>
-    /// Defines the <see cref="ICameraFocusPlusRequest" />.
-    /// </summary>
-    public interface ICameraFocusPlusRequest : ISeamosCommand
-    {
-        byte Speed { get; set; }
-    }
-}
